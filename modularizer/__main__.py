c1397223da657cf4c34e14d2369183c3bd054799
Katalin Lippa <lippa.katalin@gmail.com>
2022-06-26 17:31:20 +0200

    setting loglevel to fatal

--- modularizer/__main__.py
+++ modularizer/__main__.py
@@ -3,6 +3,6 @@ from modularizer.app import Modularizer
 from modularizer.user_interface.console import Console
 
 if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.INFO)
+    logging.getLogger().setLevel(logging.FATAL)
     ui = Console()
     app = Modularizer(ui)

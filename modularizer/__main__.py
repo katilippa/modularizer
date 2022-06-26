3fc5037358713c80e31c47d5b8e08a32033ef467
Katalin Lippa <lippa.katalin@gmail.com>
2022-06-26 17:44:15 +0200

    ignore warnings

--- modularizer/__main__.py
+++ modularizer/__main__.py
@@ -1,8 +1,11 @@
 import logging
+import warnings
+
 from modularizer.app import Modularizer
 from modularizer.user_interface.console import Console
 
 if __name__ == '__main__':
+    warnings.filterwarnings("ignore")
     logging.getLogger().setLevel(logging.FATAL)
     ui = Console()
     app = Modularizer(ui)
